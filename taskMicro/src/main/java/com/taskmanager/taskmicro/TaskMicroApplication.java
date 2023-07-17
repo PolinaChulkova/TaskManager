8f673230f23fce2d201b2aaaccc9963c99c96387
Полина <shelkova1937@gmail.com>
2023-07-17 19:23:17 +0300

    init project

---
+++ taskMicro/src/main/java/com/taskmanager/taskmicro/TaskMicroApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMicroApplication.class, args);
+	}
+
+}
