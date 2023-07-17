dcda922902a5f98fc112880bafeb84a3cc503c3b
Полина <shelkova1937@gmail.com>
2023-07-17 19:23:17 +0300

    init project

---
+++ emailSenderMicro/src/main/java/com/taskmanager/emailsendermicro/EmailSenderMicroApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.emailsendermicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSenderMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSenderMicroApplication.class, args);
+	}
+
+}
---
+++ taskMicro/src/test/java/com/taskmanager/taskmicro/TaskMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
