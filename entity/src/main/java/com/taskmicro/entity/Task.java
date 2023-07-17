dd89e97b35c126b41c844ba86cd16fb974ea1e72
Полина <shelkova1937@gmail.com>
2023-07-17 19:57:57 +0300

    create model Task

--- entity/src/main/java/com/taskmicro/entity/Task.java
+++ entity/src/main/java/com/taskmicro/entity/Task.java
@@ -1,4 +1,26 @@
 package com.taskmicro.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "usr")
+@Data
 public class Task {
+    @Id
+    @Column(name = "task_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long taskId;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "status")
+    private boolean status;
+    @Column(name = "completionDate")
+    private Date completionDate;
+    @Column(name = "user_id")
+    private Long userId;
 }
