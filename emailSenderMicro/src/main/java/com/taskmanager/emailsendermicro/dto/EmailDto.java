7d9f9de6cc4424ca2d1c42ad68301634ad78fd95
Полина <shelkova1937@gmail.com>
2023-08-03 18:41:41 +0300

    fixed problem in EmailDto

--- emailSenderMicro/src/main/java/com/taskmanager/emailsendermicro/dto/EmailDto.java
+++ emailSenderMicro/src/main/java/com/taskmanager/emailsendermicro/dto/EmailDto.java
@@ -1,9 +1,9 @@
-package com.taskmanager.emailsendermicro.model;
+package com.taskmanager.emailsendermicro.dto;
 
 import lombok.Data;
 
 @Data
-public class Email {
+public class EmailDto {
     private final String to;
     private final String subject;
     private final String text;
